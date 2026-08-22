405baf91c19c5747d056251902e2b3bb6ca348cf
ubuntu_lenovo <superyxwm@163.com>
2026-08-22 11:19:10 +0800

    Acceptor完成

--- Acceptor.cc
+++ Acceptor.cc
@@ -1,8 +1,10 @@
 #include "Acceptor.h"
 #include "logger.h"
+#include "InetAddress.h"
 
 #include <sys/socket.h>
 #include <errno.h>
+#include <unistd.h>
 
 static int createNonblocking()
 {
@@ -11,18 +13,54 @@ static int createNonblocking()
     {
         LOG_FATAL("%s:%s:%d listen socket create err:%d \n", __FILE__, __FUNCTION__, __LINE__, errno);
     }
+    return sockfd;
 }
 
 Acceptor::Acceptor(EventLoop *loop, const InetAddress &listenAddr, bool reuseport)
     : loop_(loop)
     , acceptSocket_(createNonblocking())
     , acceptChannel_(loop, acceptSocket_.fd())
+    , listenning_(false)
 {
-
+    acceptSocket_.setReuseAddr(true);
+    acceptSocket_.setReusePort(true);
+    acceptSocket_.bindAddress(listenAddr);  // bind
+    // TcpServer::start() Acceptor.listen 有新用户的连接，要执行一个回调（connfd=》channel=》subloop）
+    acceptChannel_.setReadCallback(std::bind(&Acceptor::handleRead, this));
 }
 Acceptor::~Acceptor()
 {
-
+    acceptChannel_.disableAll();
+    acceptChannel_.remove();
 }
-void Acceptor::listen();
-void Acceptor::handleRead();
+void Acceptor::listen()
+{
+    listenning_ = true;
+    acceptSocket_.listen();
+    acceptChannel_.enableReading(); // acceptChannel_ =》 Poller
+}
+// listenfd有事件发生了，就是有新用户连接了
+void Acceptor::handleRead()
+{
+    InetAddress peerAddr;
+    int connfd = acceptSocket_.accept(&peerAddr);
+    if(connfd >= 0)
+    {
+        if(newConnectionCallback_)
+        {
+            newConnectionCallback_(connfd, peerAddr); // 轮询找到subloop 唤醒分发当前新客户端的Channel
+        }
+        else
+        {
+            ::close(connfd);
+        }
+    }
+    else
+    {
+        LOG_ERROR("%s:%s:%d accept err:%d \n", __FILE__, __FUNCTION__, __LINE__, errno);
+        if(errno == EMFILE)
+        {
+            LOG_ERROR("%s:%s:%d sockfd reached limit! \n", __FILE__, __FUNCTION__, __LINE__);
+        }
+    }
+}
